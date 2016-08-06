b78c0d10c2e153a32015298a8f5a1c11ebd85a04
Alex DeCapri <alexdecapri@Alexs-MacBook-Pro.local>
2016-08-06 15:35:12 -0600

    SearchBar input visible

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyCij41njmrplmL4ike_iwAvxlbjWOHSnTY';
 
 // Create a new component.  This component should produce some HTML
 const App = () => {
-  return <div>Hi</div>;
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 // Take this component's generaeted HTML and put in on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
