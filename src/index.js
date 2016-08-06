5164cf5ba0039749c88431d9febdca73739427ee
Alex DeCapri <alexdecapri@gmail.com>
2016-08-06 16:44:28 -0600

    Create video_list and link to index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCij41njmrplmL4ike_iwAvxlbjWOHSnTY';
 
-// Create a new component.  This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos });  for ES6! :)
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList />
+      </div>
+    );
+  }
 };
 
-// Take this component's generaeted HTML and put in on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
